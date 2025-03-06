94258925b2aca5f84463ccae270270d46030a5d4
Babur Makhmudov <bmuddha13@gmail.com>
2025-03-06 20:13:52 +0400

    fix: don't remove files on append vec drop

--- accounts-db/src/append_vec.rs
+++ accounts-db/src/append_vec.rs
@@ -586,7 +586,7 @@ impl AppendVec {
             append_lock: Mutex::new(()),
             current_len: AtomicUsize::new(current_len),
             file_size,
-            remove_file_on_drop: AtomicBool::new(true),
+            remove_file_on_drop: AtomicBool::new(false),
         })
     }
 
